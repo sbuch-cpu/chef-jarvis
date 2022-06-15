4a5212a962c3b60df6bc03590f027b06aa92611a
ericabuchanan <eeobuchanan@gmail.com>
2022-06-15 16:15:16 -0400

    attempting to tokenize loaded in data

--- sandbox/retrain_attempt.py
+++ sandbox/retrain_attempt.py
@@ -1,17 +1,38 @@
 import os
 import json
+import pandas as pd
+from fine_tuning_data import get_path
+from distilbert_custom.distilBERT_attempt import get_model_and_tokenizer
+
 from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
 from torch.utils.data import random_split
-from fine_tuning_data import get_path
 
-def main():
+def load_data_to_datasets():
     # Read the existing question set json file using the json module
     module_path = get_path('chef-jarvis')
     with open(os.path.join(module_path, 'training_data/question_set.json'), 'r') as f:
         json_formatted_dataset = json.load(f)
-    #print(json_formatted_dataset)
 
+    # Read the contexts from the tokenized_recipes.csv file
+    with open(os.path.join(module_path, 'training_data/tokenized_recipes.csv'), 'r') as f:
+        contexts = pd.read_csv(f)
+
+    return json_formatted_dataset, contexts
+
+def tokenize_data(questions_dataset, contexts_dataset):
+    # Load DistilBERT model and tokenizer
+    model, tokenizer = get_model_and_tokenizer()
+    # Get questions and context from datasets
+    questions = [data['question'] for data in questions_dataset]
+    contexts = contexts_dataset['tokenized'].values
+    # Tokenize questions and contexts
+    data_encodings = tokenizer(questions, contexts, truncation=True, padding=True)
+
+
+def split_set():
+    print("coming later")
+    '''
     # Create indices for the split (80/20 train/test)
     test_split = 0.2
     dataset_size = len(json_formatted_dataset)
@@ -33,6 +54,12 @@ def main():
     print(train_loader)
     print("#################### BREAK #####################")
     print(test_loader)
+    '''
+
+def main():
+    json_dataset, recipe_context = load_data_to_datasets()
+    tokenize_data(json_dataset, recipe_context)
+    # split_set()
 
 if __name__ == "__main__":
     main()
