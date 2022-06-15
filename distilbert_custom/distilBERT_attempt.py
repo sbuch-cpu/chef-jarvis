1fd8e8d901d495b2bff6cd7366507a7c4ef4abd5
Buchanan <scottcob12@gmail.com>
2022-06-15 12:47:34 -0700

    changing how the model and tokenizer are initialized

--- distilbert_custom/distilBERT_attempt.py
+++ distilbert_custom/distilBERT_attempt.py
@@ -1,14 +1,17 @@
 import torch
 from transformers import DistilBertTokenizer, DistilBertForQuestionAnswering
 
-tokenizer = DistilBertTokenizer.from_pretrained("distilbert-base-uncased-distilled-squad")
-model = DistilBertForQuestionAnswering.from_pretrained("distilbert-base-uncased-distilled-squad")
 
-
-def ask_distilBERT(question, context):
+def get_model_and_tokenizer():
+    tokenizer = DistilBertTokenizer.from_pretrained("distilbert-base-uncased-distilled-squad")
+    model = DistilBertForQuestionAnswering.from_pretrained("distilbert-base-uncased-distilled-squad")
     special_tokens_dict = {'additional_special_tokens': ['[INGSTART]', '[INGITEM]', '[INSTSTART]', '[INSTITEM]']}
     num_added_toks = tokenizer.add_special_tokens(special_tokens_dict)
     model.resize_token_embeddings(len(tokenizer))
+    return model, tokenizer
+
+def ask_distilBERT(question, context):
+    model, tokenizer = get_model_and_tokenizer()
 
     inputs = tokenizer(question, context, return_tensors="pt")
     print(inputs['input_ids'])
@@ -24,6 +27,7 @@ def ask_distilBERT(question, context):
 
 
 def fine_tune_distilBERT(question, context):
+    model, tokenizer = get_model_and_tokenizer()
     special_tokens_dict = {'additional_special_tokens': ['[INGSTART]', '[INGITEM]', '[INSTSTART]', '[INSTITEM]']}
     num_added_toks = tokenizer.add_special_tokens(special_tokens_dict)
     model.resize_token_embeddings(len(tokenizer))
